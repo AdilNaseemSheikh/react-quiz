f076e40b340d85732d4774b74abe79cc95c44107
Adil Naseem <adil.naseem@lyftyfy.com>
2023-09-13 16:55:59 +0500

    f :wq inalized ✅

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Options from "./options";
 
-function Question({question}) {
-  return <div>
-    <h4>{question.question}</h4>
-    <Options question={question}/>
-  </div>;
+function Question({ question, answer, dispatch }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options question={question} dispatch={dispatch} answer={answer} />
+    </div>
+  );
 }
 
 export default Question;
